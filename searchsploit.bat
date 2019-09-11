432e9f8119ccd17b354a41250f99535e679eee61
Rad10 <nrc1mr.e3@gmail.com>
2019-09-11 15:57:19 -0400

    Removed color arg from interface since colors now work in windows

--- searchsploit.bat
+++ searchsploit.bat
@@ -1,4 +1,4 @@
 @echo off
 set pythonscript = ""
 :: put in the exact directory where youre storing your script. you can place this in a folder in path and youll be able to run it like on linux
-python "%pythonscript%\searchsploit.py" --colour %*
+python "%pythonscript%\searchsploit.py" %*
